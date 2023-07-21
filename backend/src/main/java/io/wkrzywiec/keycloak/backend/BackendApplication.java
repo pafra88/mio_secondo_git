f666e6afad0654fd8b472998ef05b36bd1a13fd0
paolo <p.assaiante@gmail.com>
2023-07-21 15:02:44 +0200

    commit da main

---
+++ backend/src/main/java/io/wkrzywiec/keycloak/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package io.wkrzywiec.keycloak.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+//test1
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
